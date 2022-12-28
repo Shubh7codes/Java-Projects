aa1b5121c82028acbd610c18d38d8c05f82c02c5
Shubh7codes <121547324+Shubh7codes@users.noreply.github.com>
2022-12-28 15:28:01 +0530

    Update Mortgage_Calculator.java

--- Mortgage_Calculator.java
+++ Mortgage_Calculator.java
@@ -1,3 +1,8 @@
+/* Hello in this Program 
+I made a Mortgage Calcualtor with the use of 
+Conditional statements in Java*/
+
+//Importing the required packages
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -9,36 +14,46 @@ public class Mortgage_Calculator {
         float annualInterest,monthlyInterest=0;
         Scanner sc = new Scanner(System.in);
 
+        //Block of code to ask user to enter Principal
+        // Using a Infinite Loop to validate if the data entered by user is correct
         while (true) {
                 System.out.print("Principal: ");
                 principal = sc.nextInt();
                 if(principal >=1000 && principal <= 1000000)
-                    break;
+                    break; //If conditiion is satisfied it breaks out of loop
                 System.out.println("Enter a value between 1000 and 1000000");
         }
+        
+        //Block of code to ask user to enter Annual Interest
+        // Using a Infinite Loop to validate if the data entered by user is correct
         while (true) {
                 System.out.print("Annual Interest Rate: ");
                 annualInterest = sc.nextFloat();
                 if(annualInterest >=1 && annualInterest <= 30){
                     monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-                    break;
+                    break; //If conditiion is satisfied it breaks out of loop
                 }
                 System.out.println("Enter a value between 1 and 30");
         }
+        
+        //Block of code to ask user to enter Period in Years
+        // Using a Infinite Loop to validate if the data entered by user is correct
         while (true){
                 System.out.print("Period (Years): ");
                 byte years = sc.nextByte();
                 if(years >=1 && years<=30){
                      numberOfPayments = years * MONTHS_IN_YEAR;
-                     break;
+                     break; //If conditiion is satisfied it breaks out of loop
                 }
                 System.out.println("Enter a value between 1 and 30");
         }
-
+        
+        //Calculation of Mortgage
         double mortgage = principal *
                 (monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayments))
                 /(Math.pow(1+monthlyInterest,numberOfPayments)-1);
 
+        //Using Formatted String to print currency symbol along with the calculated value
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: "+mortgageFormatted);
     }
